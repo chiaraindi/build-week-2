06a6a79f59ac218ae104cafd83aa12a9d08d11cd
Andrea <andrew.ryan.r.a@gmail.com>
2025-03-03 23:11:11 +0100

    Homepage: improved UI

--- homepage/scripts/main.js
+++ homepage/scripts/main.js
@@ -1,6 +1,6 @@
 const swiper = new Swiper('.swiper', {
     slidesPerView: 'auto',
-    spaceBetween: 15,
+    spaceBetween: 50,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
@@ -12,7 +12,7 @@ const carouselContainer = document.querySelector('.carousel-wrapper');
 
 carouselIcons.forEach(icon => {
     carouselContainer.innerHTML += `<div class="swiper-slide">
-                                            <a href="#" class="text-decoration-none text-secondary fw-medium">
+                                            <a href="#" class="text-decoration-none fw-medium">
                                             <div class="swiper-icon">
                                                 ${icon.svg}
                                             </div>
