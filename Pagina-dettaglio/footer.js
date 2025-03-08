ca6568398212c779fcd0165b4b92324c2e353182
Chiara Indirli <chiaraindirli2@gmail.com>
2025-03-08 17:34:03 +0100

    completed

---
+++ Pagina-dettaglio/footer.js
@@ -0,0 +1,50 @@
+const popolariLocations = [
+    {
+        location: 'Acireale',
+        propertyType: 'Soggiorni in appartamenti',
+    },
+    {
+        location: 'Assisi',
+        propertyType: 'Soggiorni in appartamenti',
+    },
+    {
+        location: 'Catania',
+        propertyType: 'Soggiorni in appartamenti',
+    },
+    {
+        location: 'Giulianova',
+        propertyType: 'Case vacanze',
+    },
+    {
+        location: 'Los Angeles',
+        propertyType: 'B&amp;B',
+    },
+    {
+        location: 'Marotta',
+        propertyType: 'Soggiorni in case sulla spiaggia',
+    },
+    {
+        location: 'Matera',
+        propertyType: 'Soggiorni in appartamenti',
+    },
+    {
+        location: 'Monaco',
+        propertyType: 'Soggiorni in appartamenti',
+    },
+    {
+        location: 'Palermo',
+        propertyType: 'Soggiorni in ville',
+    },
+    {
+        location: 'Parma',
+        propertyType: 'Soggiorni in appartamenti',
+    },
+    {
+        location: 'Parma',
+        propertyType: 'Soggiorni in appartamenti',
+    },
+    {
+        location: 'Parma',
+        propertyType: 'Soggiorni in appartamenti',
+    },
+];
